c787a06a133019dd6077e44078f47e6964e580f0
koka <koka.code@gmail.com>
2018-05-18 02:36:54 +0900

    feat: add some textures

--- editor/components/models/Grass.js
+++ editor/components/models/Grass.js
@@ -1,4 +1,4 @@
-export function cube() {
+export function cube(w, h) {
   const textures = [
     new THREE.MeshLambertMaterial({map:new THREE.TextureLoader().load("/grass.png")}),
     new THREE.MeshLambertMaterial({map:new THREE.TextureLoader().load("/grass_dirt.png")}),
@@ -16,9 +16,32 @@ export function cube() {
     new THREE.MeshLambertMaterial({map:new THREE.TextureLoader().load("/grass.png")}),
     new THREE.MeshLambertMaterial({map:new THREE.TextureLoader().load("/grass.png")})
   ]
-
-  const geometry = new THREE.BoxGeometry(50, 50, 50);
+  const height = h | 50
+  const width = w | 50
+  const geometry = new THREE.BoxGeometry(width, height, width);
   const material = new THREE.MeshBasicMaterial({ color: 0xb8bb26, wireframe: true })
 
   return new THREE.Mesh(geometry, t_g)
+}
+
+export function dirtyGrass(w, h) {
+  const textures = [
+    new THREE.MeshLambertMaterial({map:new THREE.TextureLoader().load("/grass.png")}),
+    new THREE.MeshLambertMaterial({map:new THREE.TextureLoader().load("/grass_dirt.png")}),
+    new THREE.MeshLambertMaterial({map:new THREE.TextureLoader().load("/grass_dirt.png")}),
+    new THREE.MeshLambertMaterial({map:new THREE.TextureLoader().load("/grass_dirt.png")}),
+    new THREE.MeshLambertMaterial({map:new THREE.TextureLoader().load("/grass_dirt.png")}),
+    new THREE.MeshLambertMaterial({map:new THREE.TextureLoader().load("/grass_dirt.png")}),
+  ]
+  const height = h | 50
+  const width = w | 50
+  const geo = new THREE.BoxGeometry(width, height, width)
+  return new THREE.Mesh(geo, textures)
+}
+
+
+export function Cloud() {
+  const geo = new THREE.BoxGeometry(50, 50, 50)
+  const mat = new THREE.MeshBasicMaterial({ color: 0xdddddd })
+  return new THREE.Mesh(geo, mat)
 }
